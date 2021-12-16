e81f8e829b437dae1d0fa0d3492dee3d9d028035
shuja <shuja.devops@gmail.com>
2021-12-16 19:40:39 +0530

    dynamic values in variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,7 +5,7 @@ c=boolean
 echo a=$a
 echo a in currency = ${a}USD
 
-DATE = 15/12/2021
+DATE=15/12/2021
 echo Good Morning, Todays date is ${DATE}
 dat=$(date +%F)
 echo Good Morning, Todays date is ${dat}
