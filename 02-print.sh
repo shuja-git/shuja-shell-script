cd0bd5b60b9ea8900fffa8a6a79784ba441ff145
shuja <shuja.devops@gmail.com>
2021-12-16 18:46:12 +0530

    echo command color blink modified script

--- 02-print.sh
+++ 02-print.sh
@@ -11,4 +11,5 @@ echo -e "\e[33mHello in Yello color\e[0m"
 echo -e "\e[34mHello in Blue color\e[0m"
 echo -e "\e[35mHello in Magenta Color \e[0m"
 echo -e "\e[36mHello in Cyan color\e[0m"
+echo -e "\e[5mShuja DevOps Engineer\e[0m"
 
