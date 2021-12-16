673697bf3eb6914049064659e6aa8d037a4a88a1
shuja <shuja.devops@gmail.com>
2021-12-16 18:39:03 +0530

    echo command color script

--- 02-print.sh
+++ 02-print.sh
@@ -2,6 +2,13 @@
 
 echo Hello
 echo Hello World
-
 echo installing Nginx
 echo starting Nginx
+# colors
+echo  -e "\e[31mHello in Red Color\e[0m"
+echo  -e "]e[32mHello in Green Color\e[0m"
+echo -e "\e[33mHello in Yello color\e[0m"
+echo -e "\e[34mHello in Blue color\e[0m"
+echo -e "\e[35mHello in Magenta Color \e[0m"
+echo -e "\e[36mHello in Cyan color\e[0m"
+
