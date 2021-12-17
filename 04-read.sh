0c952ff438600f726abe2e7668160a331c0b0815
shuja <shuja.devops@gmail.com>
2021-12-17 11:51:34 +0530

    case statement

--- 04-read.sh
+++ 04-read.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-read -p "Enter ur name : " name
-read -p "enter ur age : " age
-echo "ur name is " ${name}
-echo "ur age is" ${age}
+#read -p "Enter ur name : " name
+#read -p "enter ur age : " age
+#echo "ur name is " ${name}
+#echo "ur age is" ${age}
 #case statement
+read -p "enter component to setup" component
 case $component in
 frontend)
   echo "frontend setup"
