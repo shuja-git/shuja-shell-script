d39a09cdcfe63a082ae723cba921a74d8c2605da
shuja <shuja.devops@gmail.com>
2021-12-17 11:46:21 +0530

    case statement

--- 04-read.sh
+++ 04-read.sh
@@ -4,3 +4,17 @@ read -p "Enter ur name : " name
 read -p "enter ur age : " age
 echo "ur name is " ${name}
 echo "ur age is" ${age}
+#case statement
+case $component in
+frontend)
+  echo "frontend setup"
+  ;;
+catalogue)
+  echo "catalogue setup"
+ ;;
+user)
+  echo "user setup"
+  ;;
+*)
+  echo "u entered wrong entry"
+esac
