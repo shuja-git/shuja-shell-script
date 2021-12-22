88a9a14154601b56da91e861d101386d88958141
shuja <shuja.devops@gmail.com>
2021-12-22 20:07:13 +0530

    getting dynamic data in variable

--- examples/adduser.sh
+++ examples/adduser.sh
@@ -2,3 +2,9 @@ username=shuja
 echo "Adding user - ${username}"
 echo password | passwd --stdin ${username}
 echo "successfully added user - ${username}"
+
+DATE=22/12/2021
+echo  date is - ${DATE}
+DATE=$(date +%F)
+echo todays date is - ${DATE}
+
