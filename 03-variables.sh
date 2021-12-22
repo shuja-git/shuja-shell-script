e196676f653db33f41868cb3a52ad0dc1148670d
shuja <shuja.devops@gmail.com>
2021-12-22 19:36:53 +0530

    create variable script and declare variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,15 +2,5 @@ a=100
 b=shuja
 c=boolean
 
-echo a=$a
-echo a in currency = ${a}USD
-
-DATE=15/12/2021
-echo Good Morning, Todays date is ${DATE}
-dat=$(date +%F)
-echo Good Morning, Todays date is ${dat}
-
-echo user=${USER}
-echo A=${A}
 
 
