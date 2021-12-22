66bf838af48ce4fc6fa94a0a11633b62c3514dcf
shuja <shuja.devops@gmail.com>
2021-12-22 20:12:45 +0530

    arthmatic substitution

--- examples/adduser.sh
+++ examples/adduser.sh
@@ -8,3 +8,6 @@ echo  date is - ${DATE}
 DATE=$(date +%F)
 echo todays date is - ${DATE}
 
+echo "Adding 2 numbers so using arthmatic substitution"
+add=$((2+3))
+echo "Addition of 2 number is = ${add}"
