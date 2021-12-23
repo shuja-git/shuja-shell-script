3ffb9c851059cea37d2588a22c96117003e31800
shuja <shuja.devops@gmail.com>
2021-12-23 10:47:31 +0530

    meaning of special var

--- shell-variables.sh
+++ shell-variables.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+#${0} is the name of the script
 echo ${0}
+
+#${1} is the first argument passed
 echo ${1}
+#${2} is the 2nd argument passed
 echo  ${2}
 
+#${*} and ${@} to get all the inputs/arguments
 echo ${*}
 echo ${@}
+#${#} is the number of inputs/arguments passed
 echo  ${#}
