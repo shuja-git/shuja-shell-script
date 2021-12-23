c1796cd2d662757d7dfa36039ad222b68b2b28c6
shuja <shuja.devops@gmail.com>
2021-12-23 11:01:31 +0530

    passing 2 args

--- examples/adduser.sh
+++ examples/adduser.sh
@@ -5,8 +5,10 @@ echo "successfully added user - ${username}"
 
 DATE=22/12/2021
 echo  date is - ${DATE}
-DATE=${2}
-echo todays date is - ${DATE}
+DATE=$(date %+F)
+var=DATE
+
+echo todays date is - ${2}
 
 #echo user = ${USER}
 #echo A = ${A}
